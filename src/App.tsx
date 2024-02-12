b264a480026d509b61b11e12c6dfdb5c4e79153d
nachan28 <torajiro282828@gmail.com>
2024-02-12 20:16:45 +0900

    Add a very basic logic in App component
    Add states, functions and game clear conditions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [wolf, setWolf] = useState("right");
+  const [sheep, setSheep] = useState("right");
+  const [cabbage, setCabbage] = useState("right");
+  const [count, setCount] = useState(0);
+  const [game, setGame] = useState("playing")
+
+  const items = ["wolf", "sheep", "cabbage"];
+
+  const toTheRight = (item: string) => {
+    if (item === "wolf") {
+      setWolf("right")
+    } else if (item === "sheep") {
+      setSheep("right")
+    } else {
+      setCabbage("right")
+    }
+    setCount(count + 1);
+  }
+  const toTheLeft = (item: string) => {
+    if (item === "wolf") {
+      setWolf("left")
+    } else if (item === "sheep") {
+      setSheep("left")
+    } else {
+      setCabbage("left")
+    }
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+    if (wolf === sheep && wolf !== cabbage){
+      alert("Ah, you failed...")
+    }else if(sheep === cabbage && sheep !== wolf){
+      alert("Ah, you failed...")
+    }else if(wolf === "left" && sheep === "left" && cabbage === "left") {
+      alert("Congratulations!")
+    }
+  }, [wolf, sheep, cabbage])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      {items.map(item => (
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {item}
+          <span>
+            <button onClick={() => toTheRight(item)} >To the Right</button>
+            <button onClick={() => toTheLeft(item)}>To the Left</button>
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      ))}
+
+      <p>wolf: {wolf}</p>
+      <p>sheep: {sheep}</p>
+      <p>cabbage: {cabbage}</p>
+      <p>{count}</p>
+    </>
+  )
 }
 
 export default App;
