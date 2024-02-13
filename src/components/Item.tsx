5db94e4301b8c5423640d4b0eb1c9a5fe2bcdf7e
nachan28 <torajiro282828@gmail.com>
2024-02-13 18:02:21 +0900

    Small change.
    Add width attr to img in Item component.

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,14 +1,15 @@
 type Props = {
     src: string;
+    type: string;
     onClick: () => void;
     children: string;
 }
 
 export const Item = (props: Props) => {
-    const { src, children, onClick } = props
+    const { src, type, children, onClick } = props
     return (
         <>
-            <img src={src} alt="Sheep" width={"100px"} onClick={onClick}/>
+            <img src={src} alt={type} width={"100px"} onClick={onClick}/>
             <span>{children}</span>
         </>
     )
