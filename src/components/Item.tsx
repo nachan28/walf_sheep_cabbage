80ce292077ce0215021c173c9485f1b7fb0d5c34
nachan28 <torajiro282828@gmail.com>
2024-02-13 21:19:56 +0900

    Small change.
    Removed unused comments.

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,4 +1,4 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 type Props = {
     src: string;
@@ -8,22 +8,23 @@ type Props = {
 }
 
 export const Item = (props: Props) => {
-    const { src, type, onClick, location} = props
+    const { src, type, onClick, location } = props
+    
 
-    // アニメーションのための初期位置と最終位置を定義
-  const variants = {
-    left: { x: 0 }, // 左岸への移動
-    right: { x: 500 }, // 右岸への移動
-  };
+    // アニメーション
+    const variants = {
+        left: { x: 0 }, 
+        right: { x: 500 }, 
+    };
 
-  return (
-    <motion.div
-      initial={location} // 初期位置
-      animate={location} // アニメーション後の状態
-      variants={variants} // 位置に応じた変化
-      transition={{ duration: 0.5 }} // アニメーションの持続時間
-    >
-      <img src={src} alt={type} onClick={onClick} width="100px"/>
-    </motion.div>
-  )
+    return (
+        <motion.div
+            initial={location}
+            animate={location}
+            variants={variants}
+            transition={{ duration: 0.5 }}
+        >
+            <img src={src} alt={type} onClick={onClick} width="100px" />
+        </motion.div>
+    )
 }
