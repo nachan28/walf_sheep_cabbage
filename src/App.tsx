ae93f39bf6fd88214b826708d7a8bcc4c0358de7
nachan28 <torajiro282828@gmail.com>
2024-02-13 09:57:45 +0900

    Fix whole logic
    Bind each state into an object. Add functions: handleGo, handleReset, isGameOver, isGameWon.

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,75 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+type Locations = {
+  wolf: string;
+  sheep: string;
+  cabbage: string;
+  boat: string;
+}
+
 function App() {
-  const [wolf, setWolf] = useState("right");
-  const [sheep, setSheep] = useState("right");
-  const [cabbage, setCabbage] = useState("right");
-  const [count, setCount] = useState(0);
-  const [game, setGame] = useState("playing")
+  const [locations, setLocations] = useState({
+    wolf: "right",
+    sheep: "right",
+    cabbage: "right",
+    boat: "right",
+  })
+  const [selectedItem, setSelectedItem] = useState<"wolf" | "sheep" | "cabbage" | null>(null)
 
-  const items = ["wolf", "sheep", "cabbage"];
+  const handleGo = (item: "wolf" | "sheep" | "cabbage" | null) => {
+    const newLocations = { ...locations }
+    if (item) {
+      newLocations[item] = newLocations[item] === "right" ? "left" : "right";
+    }
+    newLocations.boat = newLocations.boat === "right" ? "left" : "right";
 
-  const toTheRight = (item: string) => {
-    if (item === "wolf") {
-      setWolf("right")
-    } else if (item === "sheep") {
-      setSheep("right")
-    } else {
-      setCabbage("right")
+    if (isGameOver(newLocations)) {
+      alert("Game over")
+    } else if (isGameWon(newLocations)) {
+      alert("Congratulations! You won!")
     }
-    setCount(count + 1);
-  }
-  const toTheLeft = (item: string) => {
-    if (item === "wolf") {
-      setWolf("left")
-    } else if (item === "sheep") {
-      setSheep("left")
-    } else {
-      setCabbage("left")
-    }
-    setCount(count + 1);
+    setLocations(newLocations);
   }
 
-  useEffect(() => {
-    if (wolf === sheep && wolf !== cabbage){
-      alert("Ah, you failed...")
-    }else if(sheep === cabbage && sheep !== wolf){
-      alert("Ah, you failed...")
-    }else if(wolf === "left" && sheep === "left" && cabbage === "left") {
-      alert("Congratulations!")
-    }
-  }, [wolf, sheep, cabbage])
+  const handleReset = () => {
+    setLocations({
+      wolf: "right",
+      sheep: "right",
+      cabbage: "right",
+      boat: "right",
+    });
+    setSelectedItem(null);
+  }
 
+  const isGameOver = (locations: Locations) => {
+    if (locations.wolf === locations.sheep && locations.boat !== locations.wolf && locations.cabbage !== locations.wolf) {
+      return true
+    }
+    if (locations.sheep === locations.cabbage && locations.boat !== locations.sheep && locations.wolf !== locations.sheep) {
+      return true;
+    }
+    return false;
+  }
+
+  const isGameWon = (locations: Locations) => {
+    if (locations.wolf === "left" && locations.sheep === "left" && locations.cabbage === "left") {
+      return true
+    }
+    return false
+  }
   return (
     <>
-      {items.map(item => (
-        <p>
-          {item}
-          <span>
-            <button onClick={() => toTheRight(item)} >To the Right</button>
-            <button onClick={() => toTheLeft(item)}>To the Left</button>
-          </span>
-        </p>
-      ))}
+      <p onClick={() => setSelectedItem("wolf")}>wolf: {locations.wolf}</p>
+      <p onClick={() => setSelectedItem("sheep")}>sheep: {locations.sheep}</p>
+      <p onClick={() => setSelectedItem("cabbage")}>cabbage: {locations.cabbage}</p>
+      <p>boat: {locations.boat}</p>
+      <br />
+      <p>selected: {selectedItem}</p>
 
-      <p>wolf: {wolf}</p>
-      <p>sheep: {sheep}</p>
-      <p>cabbage: {cabbage}</p>
-      <p>{count}</p>
+      <button onClick={() => handleGo(selectedItem)}>GO!</button>
+      <button onClick={() => setSelectedItem(null)}>Select None</button>
+      <button onClick={() => handleReset()}>最初から</button>
     </>
   )
 }
