28071e01ad9200efa320922e50e362aae93a0bb7
nachan28 <torajiro282828@gmail.com>
2024-02-14 15:54:35 +0900

    Make Boat component droppable. It accepts wolf, sheep and cabbage

--- src/components/Boat.tsx
+++ src/components/Boat.tsx
@@ -1,5 +1,42 @@
+import { useDrop } from "react-dnd";
+
 export const Boat = () => {
+    const [{ isOver }, drop] = useDrop(
+        () => ({
+            accept: ["wolf", "sheep", "cabbage"],
+            drop: () => handleItemDropped(),
+            collect: monitor => ({
+                isOver: !!monitor.isOver()
+            })
+        })
+    )
+
+    const handleItemDropped = () => {
+        console.log("item dropped!")
+    }
     return (
-        <img src="/boat.png" alt="boat" width="100px"/>
+        <div
+            ref={drop}
+            style={{
+                position: 'relative',
+                width: '100%',
+                height: '100%',
+            }}
+        >
+            {isOver && (
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            height: '100%',
+            width: '100%',
+            zIndex: 1,
+            backgroundColor: 'yellow',
+          }}
+        />
+      )}
+            <img src="/boat.png" alt="boat" width="200px" />
+        </div>
     )
 }
