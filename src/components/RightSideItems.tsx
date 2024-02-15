57dfa02ca7a17585693f679460ac513210e497d7
nachan28 <torajiro282828@gmail.com>
2024-02-15 13:36:38 +0900

    Display each item image

--- src/components/RightSideItems.tsx
+++ src/components/RightSideItems.tsx
@@ -1,3 +1,5 @@
+import { Item } from "./Item"
+
 export type SideItemProps = {
     items: string[]
 }
@@ -6,15 +8,21 @@ export const RightSideItems = (items: SideItemProps) => {
     return (
         <div className="right-item-container" style={{
             position: "fixed",
-            top: "0", 
+            top: "0",
             right: "0",
             margin: "20px"
         }}>
             <p>RightSide</p>
             {items.items.map((item, index) => (
-                <div key={index} className="right-item">
-                    {item}
-                </div>
+                <>
+                    <div key={index} className="right-item">
+                        {item}
+                        <img src={item === "wolf" ? "/wolf.png" :
+                            item === "sheep" ? "/sheep.png" : "/cabbage.png"}
+                            alt={item}
+                            width="100px"/>
+                    </div>
+                </>
             ))}
         </div>
     )
